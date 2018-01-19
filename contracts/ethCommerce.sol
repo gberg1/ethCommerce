49c77d58b746d9ae087e6a74623827bfed36e665
David Goldberg <drgold1988@gmail.com>
2018-01-19 14:24:26 -0800

    Adds buyer and buyArticle

--- contracts/ethCommerce.sol
+++ contracts/ethCommerce.sol
@@ -3,12 +3,24 @@ pragma solidity ^0.4.11;
 contract ethCommerce {
   // State variables
   address seller;
+  address buyer;
   string name;
   string description;
   uint256 price;
 
   // events
-  event sellArticleEvent(address indexed _seller, string _name, uint256 _price);
+  event sellArticleEvent (
+    address indexed _seller,
+    string _name,
+    uint256 _price
+  );
+
+  event buyArticleEvent (
+    address indexed _seller,
+    address indexed_buyer,
+    string _name,
+    uint256 _price
+  );
 
   // sell an article
   function sellArticle(string _name, string _description, uint256 _price) public {
@@ -20,8 +32,29 @@ contract ethCommerce {
   }
 
   // get an article
-  function getArticle() public constant returns (address _seller, string _name, string _description, uint256 _price) {
-      return (seller, name, description, price);
+  function getArticle() public constant returns (
+    address _seller,
+    address _buyer,
+    string _name,
+    string _description,
+    uint256 _price) {
+    return (seller, buyer, name, description, price);
+  }
+
+  function buyArticle() payable public {
+    // Ensure there is an article for sale
+    require(seller != 0x0);
+    // Ensure article not already Sold
+    require(buyer == 0x0);
+    // Ensure seller doesn't buy own article
+    require(msg.sender != seller);
+    // Ensure value sent corresponds to the artidcle price
+    require(msg.value == price);
+    // Store buyer info
+    buyer = msg.sender;
+    // Purchase article
+    seller.transfer(msg.value);
+    buyArticleEvent(seller, buyer, name, price);
   }
 
 }
