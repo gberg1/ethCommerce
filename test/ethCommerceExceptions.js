f5b2adb9e5921fef3700edc0e273524774bfbcc1
David Goldberg <drgold1988@gmail.com>
2018-01-23 23:15:24 -0800

    Adds test coverage for list of articles

--- test/ethCommerceExceptions.js
+++ test/ethCommerceExceptions.js
@@ -1,113 +1,143 @@
-var ethCommerce = artifacts.require("./ethCommerce.sol");
+var ethCommerce = artifacts.require('./ethCommerce.sol');
 
 // Test suite
 contract('ethCommerce', function(accounts) {
   var ethCommerceInstance;
   var seller = accounts[1];
   var buyer = accounts[2];
-  var articleName = "article 1";
-  var articleDescription = "Description for article 1";
+  var articleId = 1;
+  var articleName = 'article 1';
+  var articleDescription = 'Description for article 1';
   var articlePrice = 10;
 
-  // Test case: no article for sale yet
-  it("should throw an exception if you try to buy an article when there is no article for sale", function() {
+  it('should throw an exception if you try to get articles for sale when there is no article at all', function() {
     return ethCommerce.deployed().then(function(instance) {
-      ethCommerceInstance = instance;
-      return ethCommerceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice, "ether")
-      });
-    }).then(assert.fail) // Test that call throws exception
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return ethCommerceInstance.getArticle.call();
-    }).then(function(data) {
-      // Make sure sure the contract state was not altered
-      assert.equal(data[0], 0x0, "seller must be empty");
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], '', "article name must empty");
-      assert.equal(data[3], '', "article description must be empty");
-      assert.equal(data[4].toNumber(), 0, "article price must be 0");
-    });
+        ethCommerceInstance = instance;
+        return ethCommerceInstance.getArticlesForSale();
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      })
   });
 
-  // Test case: buying an article you are selling
-  it("should throw an exception if you try to buy your own article", function() {
+  it('should throw an exception if you try to buy an article when there is no article for sale', function() {
     return ethCommerce.deployed().then(function(instance) {
-      ethCommerceInstance = instance;
-      return ethCommerceInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {
-        from: seller
+        ethCommerceInstance = instance;
+        return ethCommerceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice, 'ether')
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      }).then(function() {
+        return ethCommerceInstance.getNumberOfArticles();
+      }).then(function(data) {
+        //make sure sure the contract state was not altered
+        assert.equal(data.toNumber(), 0, 'number of articles must be zero');
       });
-    }).then(function(receipt) {
-      return ethCommerceInstance.buyArticle({
-        from: seller,
-        value: web3.toWei(articlePrice, "ether")
-      });
-    }).then(assert.fail) // Test that call throws exception
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return ethCommerceInstance.getArticle.call();
-    }).then(function(data) {
-      //make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
   });
 
-  // Test case: incorrect value
-  it("should throw an exception if you try to buy an article for a value different from its price", function() {
+  it('should throw an exception if you try to buy an article that does not exist', function() {
     return ethCommerce.deployed().then(function(instance) {
-      ethCommerceInstance = instance;
-      return ethCommerceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice + 1, "ether")
+        ethCommerceInstance = instance;
+        return ethCommerceInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, 'ether'), {
+          from: seller
+        });
+      }).then(function(receipt) {
+        return ethCommerceInstance.buyArticle(2, {
+          from: buyer,
+          value: web3.toWei(articlePrice, 'ether')
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      }).then(function() {
+        // Ensure contract state was not altered from call to sellArticle
+        return ethCommerceInstance.articles(articleId);
+      }).then(function(data) {
+        assert.equal(data[0].toNumber(), articleId, 'article id must be ' + articleId);
+        assert.equal(data[1], seller, 'seller must be ' + seller);
+        assert.equal(data[2], 0x0, 'buyer must be empty');
+        assert.equal(data[3], articleName, 'article name must be ' + articleName);
+        assert.equal(data[4], articleDescription, 'article description must be ' + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, 'ether'), 'article price must be ' + web3.toWei(articlePrice, 'ether'));
       });
-    }).then(assert.fail) // Test that call throws exception
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return ethCommerceInstance.getArticle.call();
-    }).then(function(data) {
-      // Make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], 0x0, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
   });
 
-  // Test case: article has already been sold
-  it("should throw an exception if you try to buy an article that has already been sold", function() {
+  it('should throw an exception if you try to buy your own article', function() {
     return ethCommerce.deployed().then(function(instance) {
-      ethCommerceInstance = instance;
-      return ethCommerceInstance.buyArticle({
-        from: buyer,
-        value: web3.toWei(articlePrice, "ether")
+        ethCommerceInstance = instance;
+        // Already called sellArticle method on same articleId with same seller in previous test
+        return ethCommerceInstance.buyArticle(articleId, {
+          from: seller,
+          value: web3.toWei(articlePrice, 'ether')
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      }).then(function() {
+        return ethCommerceInstance.articles(articleId);
+      }).then(function(data) {
+        // Ensure contract state was not altered by call to buyArticle
+        assert.equal(data[0].toNumber(), articleId, 'article id must be ' + articleId);
+        assert.equal(data[1], seller, 'seller must be ' + seller);
+        assert.equal(data[2], 0x0, 'buyer must be empty');
+        assert.equal(data[3], articleName, 'article name must be ' + articleName);
+        assert.equal(data[4], articleDescription, 'article description must be ' + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, 'ether'), 'article price must be ' + web3.toWei(articlePrice, 'ether'));
       });
-    }).then(function() {
-      return ethCommerceInstance.buyArticle({
-        from: web3.eth.accounts[0],
-        value: web3.toWei(articlePrice, "ether")
+  });
+
+  it('should throw an exception if you try to buy an article for a value different from its price', function() {
+    return ethCommerce.deployed().then(function(instance) {
+        ethCommerceInstance = instance;
+        return ethCommerceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice + 1, 'ether')
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      }).then(function() {
+        return ethCommerceInstance.articles(articleId);
+      }).then(function(data) {
+        // Ensure contract state was not altered by call to buyArticle
+        assert.equal(data[0].toNumber(), articleId, 'article id must be ' + articleId);
+        assert.equal(data[1], seller, 'seller must be ' + seller);
+        assert.equal(data[2], 0x0, 'buyer must be empty');
+        assert.equal(data[3], articleName, 'article name must be ' + articleName);
+        assert.equal(data[4], articleDescription, 'article description must be ' + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, 'ether'), 'article price must be ' + web3.toWei(articlePrice, 'ether'));
       });
-    }).then(assert.fail) // Test that call throws exception
-    .catch(function(error) {
-      assert(error.message.indexOf('invalid opcode') >= 0, error.message.indexOf('invalid opcode') >= 0);
-    }).then(function() {
-      return ethCommerceInstance.getArticle.call();
-    }).then(function(data) {
-      // Make sure sure the contract state was not altered
-      assert.equal(data[0], seller, "seller must be " + seller);
-      assert.equal(data[1], buyer, "buyer must be empty");
-      assert.equal(data[2], articleName, "article name must be " + articleName);
-      assert.equal(data[3], articleDescription, "article description must be " + articleDescription);
-      assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), "article price must be " + web3.toWei(articlePrice, "ether"));
-    });
+  });
+
+  it('should throw an exception if you try to buy an article that has already been sold', function() {
+    return ethCommerce.deployed().then(function(instance) {
+        ethCommerceInstance = instance;
+        return ethCommerceInstance.buyArticle(articleId, {
+          from: buyer,
+          value: web3.toWei(articlePrice, 'ether')
+        });
+      }).then(function() {
+        return ethCommerceInstance.buyArticle(articleId, {
+          from: web3.eth.accounts[0],
+          value: web3.toWei(articlePrice, 'ether')
+        });
+      }).then(assert.fail)
+      .catch(function(error) {
+        assert(error.message.indexOf('invalid opcode') >= 0, 'error message must contain invalid opcode');
+      }).then(function() {
+        return ethCommerceInstance.articles(articleId);
+      }).then(function(data) {
+        // Ensure contract state was not altered by call to buyArticle
+        assert.equal(data[0].toNumber(), articleId, 'article id must be ' + articleId);
+        assert.equal(data[1], seller, 'seller must be ' + seller);
+        assert.equal(data[2], buyer, 'buyer must be ' + buyer);
+        assert.equal(data[3], articleName, 'article name must be ' + articleName);
+        assert.equal(data[4], articleDescription, 'article description must be ' + articleDescription);
+        assert.equal(data[5].toNumber(), web3.toWei(articlePrice, 'ether'), 'article price must be ' + web3.toWei(articlePrice, 'ether'));
+      });
   });
 
 });
